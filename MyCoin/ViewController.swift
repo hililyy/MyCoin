c9ef539f8cbd6bd5dfc95fb96a0ee40817f988fe
hililyy <joun406@gmail.com>
2022-06-28 17:45:37 +0900

    commit

---
+++ MyCoin/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyCoin
+//
+//  Created by 강조은 on 2022/06/28.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
